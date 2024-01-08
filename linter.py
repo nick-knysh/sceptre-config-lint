e046bc454d45f7d12af1cf6c7b549c57c1495eec
nick-knysh <mykola.knysh@tr.com>
2024-01-08 10:07:12 -0500

    xtra option for skips

--- linter.py
+++ linter.py
@@ -116,8 +116,11 @@ def load_var_file(file_path):
             raise exc
 
 
-def validate_template(filename):
-    (args, temple_filenames, formatter) = cfnlint.core.get_args_filenames([filename])
+def validate_template(filename, ignore_checks):
+    cfn_args = [filename]
+    if (ignore_checks):
+        cfn_args.extend(['-i', ignore_checks])
+    (args, temple_filenames, formatter) = cfnlint.core.get_args_filenames(cfn_args)
     matches = list(cfnlint.core.get_matches(temple_filenames, args))
     error_msg = formatter.print_matches(matches, cfnlint.core.get_used_rules(), temple_filenames)
     exit_code = cfnlint.core.get_exit_code(matches, args.non_zero_exit_code)
@@ -151,7 +154,7 @@ def match_params(config_params, template_params):
         return 0, None
 
 
-def process_config(config_filename, variables_filename, project_home, is_try_default_template_path=True):
+def process_config(config_filename, variables_filename, project_home, ignore_checks, is_try_default_template_path=True):
     try:
         config = get_config_data(config_filename, variables_filename)
         template_path = os.path.join(project_home, config['template_path'])
@@ -159,7 +162,7 @@ def process_config(config_filename, variables_filename, project_home, is_try_def
             alt_template_path = os.path.join(project_home, 'templates', config['template_path'])
             if os.path.exists(alt_template_path) and os.path.isfile(alt_template_path):
                 template_path = alt_template_path
-        template_exit_code, template_errors = validate_template(template_path)
+        template_exit_code, template_errors = validate_template(template_path, ignore_checks)
         params_exit_code, params_errors = match_params(
             get_template_params(template_path), get_template_params(template_path)
         )
@@ -201,6 +204,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Parse parameters from a CloudFormation template and config file.")
     parser.add_argument("-c", "--config", required=False, help="Relative path to selected config YAML file.")
     parser.add_argument("-s", "--skip", required=False, help="Comma-delimited Relative paths to skipped config YAML file.")
+    parser.add_argument("-i", "--ignore", required=False, help="Comma-delimited cfn-lint ignore checks.")
     parser.add_argument("project_home", nargs=1, default=None, help="Path to the config YAML file with parameters.")
     args = parser.parse_args()
     print(f"project_home_dir: {args.project_home[0]}")
@@ -222,7 +226,7 @@ if __name__ == '__main__':
         skip_files.extend(args.skip.split(','))
     for config_path in filter(lambda x: not (next(filter(lambda skip: x.endswith(skip), skip_files), False)), configs):
         # print(f"Validating config: {config_path}")
-        comp_exit_code, error_msg = process_config(os.path.join(project_home, 'config', config_path), variables, project_home)
+        comp_exit_code, error_msg = process_config(os.path.join(project_home, 'config', config_path), variables, project_home, args.ignore)
         if error_msg:
             print(f'\nValidation failed for [{config_path}] with following errors:')
             print(error_msg)
