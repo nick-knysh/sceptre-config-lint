33c2107a4bcccaba4e7f9b810e9ecbd21a40f3fe
nick-knysh <mykola.knysh@tr.com>
2024-02-09 12:21:31 -0500

    path support. call config check

--- linter.py
+++ linter.py
@@ -13,6 +13,7 @@ import requests
 import yaml
 from jinja2 import Template
 from cfnlint.decode import cfn_yaml
+from pathspec import PathSpec
 
 # methods:
 # load_config, get_local_content, get_url_content, get_content
@@ -130,18 +131,19 @@ def get_template_params(filename):
     return cfn_yaml.load(filename)['Parameters']
 
 def get_config_data(config_filename, variables):
-    config = load_config(config_filename, variables)
-    return config
+    return load_config(config_filename, variables)
 
 def match_params(config_params, template_params):
     # get params set's diff
     config_params_set = set(config_params.keys())
+    # print(f"config_params_set={config_params_set}")
     template_params_set = set(template_params.keys())
+    # print(f"template_params_set={template_params_set}")
     conf_xtra_error = config_params_set - template_params_set
-    tpl_xtra = config_params_set - template_params_set
+    tpl_xtra = template_params_set - config_params_set
     tpl_missing = []
     for miss in tpl_xtra:
-        if 'Default' not in template_params[miss]:
+        if 'Default' not in template_params.get(miss, {}):
             tpl_missing.append(miss)
     if conf_xtra_error or tpl_missing:
         errors = []
@@ -154,28 +156,35 @@ def match_params(config_params, template_params):
         return 0, None
 
 
-def process_config(config_filename, variables_filename, project_home, linter_options, is_try_default_template_path=True):
+def process_config(config_filename, variables_filename, project_home, linter_options, is_try_default_template_path=True, verbose: bool = False):
     try:
         config = get_config_data(config_filename, variables_filename)
         template_path = os.path.join(project_home, config['template_path'])
+        rel_config_path = config_filename[len(project_home) + 1:]
+        rel_template_path = template_path[len(project_home) + 1:]
+        if verbose:
+            print(f"-- Processing config file: {rel_config_path}\t|\t[template:{rel_template_path}]")
         if not os.path.exists(template_path) and is_try_default_template_path:
             alt_template_path = os.path.join(project_home, 'templates', config['template_path'])
             if os.path.exists(alt_template_path) and os.path.isfile(alt_template_path):
                 template_path = alt_template_path
         template_exit_code, template_errors = validate_template(template_path, linter_options)
+        # template_exit_code, template_errors = 0, ""
         params_exit_code, params_errors = match_params(
-            get_template_params(template_path), get_template_params(template_path)
+            config['parameters'], get_template_params(template_path)
         )
         exit_code = max(template_exit_code, params_exit_code)
-        if exit_code != 0:
+        if exit_code != 0 and (template_errors or params_errors):
             errors = []
             if template_errors:
+                errors.append(f"FAILED: >> template errors for {rel_template_path}:")
                 errors.append(template_errors)
             if params_errors:
+                errors.append(f"FAILED: >>> config params errors for {rel_config_path}:")
                 errors.append(params_errors)
             return exit_code, '\n'.join(errors)
         else:
-            return 0, None
+            return 0, "OK"
     except Exception as e:
         return 1, f"Failed to process config file: {config_filename}: {e}"
 
@@ -196,12 +205,26 @@ def collect_configs(config_directory):
     collect_files_in(config_directory)
     return file_paths
 
+def list_files_recursively(root_dir, include_filters, skip_filters):
+    file_list = []
+    spec = PathSpec.from_lines("gitwildmatch", include_filters)
+    skip_spec = PathSpec.from_lines("gitwildmatch", skip_filters)
+    for root, dirs, files in os.walk(root_dir):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)[len(root_dir) + 1:]
+            # print("checking file: ", file_path)
+            matches_includes = not spec or spec.match_file(str(file_path))
+            if matches_includes and not (skip_spec and skip_spec.match_file(str(file_path))):
+                file_list.append(file_path)
+    return file_list
+
 
 if __name__ == '__main__':
     CONFIG_DIR = 'config'
     VARIABLES_FILENAME = 'variables.yaml'
 
     parser = argparse.ArgumentParser(description="Parse parameters from a CloudFormation template and config file.")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", required=False, help="Verbose mode.")
     parser.add_argument("-c", "--config", required=False, help="Relative path to selected config YAML file.")
     parser.add_argument("-s", "--skip", required=False, help="Comma-delimited Relative paths to skipped config YAML file.")
     parser.add_argument("-o", "--linter-options", required=False, help="cfn-lint options string")
@@ -225,24 +248,33 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join(project_home, CONFIG_DIR)):
         print("no config folder to process")
         sys.exit(0)
-    configs = collect_configs(os.path.join(project_home, CONFIG_DIR))
-    if config_filter:
-        configs = list(filter(lambda x: x.endswith(config_filter), configs))
-    skip_files = ['config.yaml']
+    skip_files = ['**/config.yaml']
     if args.skip:
         skip_files.extend(args.skip.split(','))
-    for config_path in filter(lambda x: not (next(filter(lambda skip: x.endswith(skip), skip_files), False)), configs):
+    configs = list_files_recursively(
+        os.path.join(project_home, CONFIG_DIR),
+        config_filter.split(',') if config_filter else ['**/*.yaml', '**/*.yml'],
+        skip_files
+    )
+
+    for config_path in configs:
         # print(f"Validating config: {config_path}")
         comp_exit_code, error_msg = process_config(
             os.path.join(project_home, 'config', config_path), 
             variables, 
             project_home, 
             " ".join(list(filter(None, ["-i " + args.linter_ignore_options if args.linter_ignore_options else None, args.linter_options]))), 
-            True
+            is_try_default_template_path=True,
+            verbose=args.verbose
         )
-        if error_msg:
-            print(f'\nValidation failed for [{config_path}] with following errors:')
+        if comp_exit_code > 0:
+            print(f'\n[!] Validation failed for [{config_path}] with following errors:')
             print(error_msg)
+        else:
+            if error_msg and args.verbose:
+                print(error_msg)
+
+    print(f"configs checked: {len(configs)}")
     sys.exit(comp_exit_code)
 
 
